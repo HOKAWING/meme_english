d6d4560ede9b788c03af534cb7f1d602e020a6ff
PaulHo123 <paul.ho@toppwork.com>
2023-08-03 18:35:47 +0800

    no message

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect } from "react";
 
 export default function HomePage() {
     const router = useRouter();
