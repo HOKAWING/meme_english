8df0af018b33aaa0b3c218db86378c97a00b22a2
PaulHo123 <paul.ho@toppwork.com>
2023-08-07 22:02:33 +0800

    no message

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 export default function HomePage() {
     const router = useRouter();
     useEffect(() => {
-        router.push("/login");
+        router.push("/chillkidsclub");
     }, []);
 
 }
